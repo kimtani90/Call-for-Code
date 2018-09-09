4e6a6d83297985fbfa44488e3c7bd3086e07e187
siddharth <siddharth.suthar053@gmail.com>
2018-09-09 02:45:42 -0700

    Updated the provider with comments

--- frontend/src/components/Provider.js
+++ frontend/src/components/Provider.js
@@ -1,57 +1,82 @@
 import React, {Component} from 'react';
 import {Map,Marker, GoogleApiWrapper} from 'google-maps-react';
-import { Card, CardText, CardBody,
+import { Card, CardText, CardBody,CardImg,
     CardTitle, CardSubtitle,Button} from 'reactstrap';
 
+import './provider.css';
 export class Provider extends Component{
 
+    state = {
+        person : ''
+}
     componentWillMount(){
 
         //call the api with the phone number
-        
+
+        const phone = window.location.search.substring(10);
+        //make an api call with the variable phone number
+        //get all the data in response from the request and store it in state
+        //store the returned array in person state as objects.
+    }
+
+
+    // marker(){
+    //
+    //     return this.props.state.markers.map(marker, index) =>{    //this is error because marker is empty and not an array
+    //         return(
+    //             <Marker onClick = {()=>handleSubmit(index)}
+    //                     position = {marker}/>
+    //         )
+    //     }
+    //
+    // }
+
+
+    handleSubmit(index){
+
+        //make an api call with the payload to the back end with the data as "this.state.markers[index]"
 
     }
 
+
     render(){
-
-        const triangleCoords = [
-            {lat: 25.774, lng: -80.190},
-            {lat: 18.466, lng: -66.118},
-            {lat: 32.321, lng: -64.757},
-            {lat: 25.774, lng: -80.190}
-        ];
-
         return(
 
-            <div className="col-md-6">
-                <Card>
-                    <Map google={this.props.google}
-                         style={{width: '100%', height: '100%', position: 'relative'}}
-                         className={'map'}
-                         zoom={40}>
-                        <Marker
-                            title={'The marker`s title will appear as a tooltip.'}
-                            name={'SOMA'}
-                            position={triangleCoords[0]} />
-                        <Marker
-                            name={'Dolores park'}
-                            position={triangleCoords[1]} />
-                        <Marker />
-                        <Marker
-                            name={'Your position'}
-                            position={triangleCoords[2]}
-                        />
-                        <Marker/>
-                    </Map>
-                    <CardBody>
-                        <CardTitle>Card title</CardTitle>
-                        <CardSubtitle>Card subtitle</CardSubtitle>
-                        <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                        <Button>Button</Button>
-                    </CardBody>
-                </Card>
-            </div>
+            <div className="wrapp">
 
+                <br/>
+                <h2> Select the person you want to help </h2>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+
+
+
+           <div className="row">
+               <div className="col-md-2">
+
+               </div>
+                <div className="col-md-8">
+                    <Card>
+                        <CardTitle>
+                            Select the person you want to help
+                        </CardTitle>
+
+                        <Map google={this.props.google}
+                             style={{width: '100%', height: '100%', position: 'relative'}}
+                             className={'map'}
+                             zoom={20}>
+                            {/*this.marker  uncomment his when you have the object*/}
+                        </Map>
+                        <CardBody>
+                        </CardBody>
+                    </Card>
+                </div>
+           </div>
+            </div>
 
 
 
